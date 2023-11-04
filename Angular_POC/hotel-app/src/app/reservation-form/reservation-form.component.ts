12475001b77a6daa390c9079cdca607d1074e62b
sijimoncr <sijimoncr@gmail.com>
2023-11-04 14:33:36 -0500

    Update made to Angular POC and Readme file

--- Angular_POC/hotel-app/src/app/reservation-form/reservation-form.component.ts
+++ Angular_POC/hotel-app/src/app/reservation-form/reservation-form.component.ts
@@ -1,5 +1,8 @@
 import { Component,OnInit } from '@angular/core';
-import { FormBuilder,FormGroup,Validator, Validators } from '@angular/forms';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+import { Router,ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-form',
@@ -12,7 +15,7 @@ export class ReservationFormComponent implements OnInit{
 
   reservationForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder){
+  constructor(private formBuilder: FormBuilder, private reservationService:ReservationService,private router:Router,private activatedRoute: ActivatedRoute){
 
   }
 
@@ -25,11 +28,35 @@ export class ReservationFormComponent implements OnInit{
       roomNumber: ['',Validators.required]
     })
 
+    let id = this.activatedRoute.snapshot.paramMap.get('id')
+
+    if(id){
+      let reservation = this.reservationService.getReservation(id);
+      if(reservation){
+        this.reservationForm.patchValue(reservation);
+      }      
+    }
+
+
+
   }
   
   onSubmit(){
     if(this.reservationForm.valid){
+      let reservation:Reservation = this.reservationForm.value;
 
+      let id = this.activatedRoute.snapshot.paramMap.get('id')
+      if(id){
+        //Update
+        this.reservationService.updateReservation(id,reservation);
+      }else{
+        //New Add
+        this.reservationService.addReservation(reservation);
+      }
+
+      
+
+      this.router.navigate(['/list'])
     }
   }
 
